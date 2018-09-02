270dedc04d038ae9c29753b82826712a5b126607
jason <jason@byzantineanchor-photontorpedo.gchq.org.uk>
2018-09-02 11:11:36 +0100

    Clean up the index page.

--- app.py
+++ app.py
@@ -7,11 +7,8 @@ def homepage():
     the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
 
     return """
-    <h1>Hello heroku</h1>
-    <p>It is currently {time}.</p>
-
-    <img src="http://loremflickr.com/600/400">
-    """.format(time=the_time)
+    <h1>Hello, World</h1>
+    """
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
