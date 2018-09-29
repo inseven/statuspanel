6449ee51d9d40e7434c922d77d0fed77f8177bf9
Jason Barrie Morley <jbmorley@users.noreply.github.com>
2018-09-29 09:18:58 -0700

    Remove unnecessary code.

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
 from flask import Flask
-from datetime import datetime
+
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
-
     return """
     <h1>Hello, World</h1>
     """
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=True)
+    app.run()
+
