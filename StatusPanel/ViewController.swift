15eead83b7531842f99c4e69756a662e3080e74e
Jason Barrie Morley <jason.morley@inseven.co.uk>
2018-10-03 23:57:38 -0700

    Remove unnecessary function.

--- StatusPanel/ViewController.swift
+++ StatusPanel/ViewController.swift
@@ -165,9 +165,5 @@ class ViewController: UIViewController {
 
     }
 
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
-	}
 }
 
