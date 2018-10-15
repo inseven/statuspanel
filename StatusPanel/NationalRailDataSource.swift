dc375c441a2743f0c7e7979a8189f0a2d50753e5
Tom Sutcliffe <tomsci@me.com>
2018-10-15 15:13:51 +0100

    Added NationalRailDataSource.swift

---
+++ StatusPanel/NationalRailDataSource.swift
@@ -0,0 +1,81 @@
+//
+//  NationalRailDataSource.swift
+//  StatusPanel
+//
+//  Created by Tom Sutcliffe on 24/09/2018.
+//  Copyright © 2018 Tom Sutcliffe. All rights reserved.
+//
+
+import Foundation
+
+class NationalRailDataSource : DataSource {
+	// See https://wiki.openraildata.com/index.php/NRE_Darwin_Web_Service_(Public)
+	// and https://lite.realtime.nationalrail.co.uk/OpenLDBWS/
+	// As implemented by https://huxley.unop.uk/ because the raw national rail API is so bad
+	let token = "KEY"
+
+	let targetCrs = "CBG"
+
+	var dataItems = [DataItem]()
+	var completion: DataSource.Callback?
+
+	func get<T>(_ what: String, onCompletion: @escaping (T?, Error?) -> Void) where T : Decodable {
+		let sep = what.contains("?") ? "&" : "?"
+		let url = URL(string: "https://huxley.apphb.com/" + what + sep + "accessToken=\(token)")!
+		JSONRequest.makeRequest(url: url, onCompletion: onCompletion)
+	}
+
+	func fetchData(onCompletion: @escaping Callback) {
+		completion = onCompletion
+		get("delays/KGX/to/\(targetCrs)/10", onCompletion: gotDelays)
+	}
+
+	struct Delays: Decodable {
+		var delays: Bool
+		var totalTrainsDelayed: Int
+		var totalDelayMinutes: Int
+		var delayedTrains: [Delay]
+
+		struct Delay: Codable {
+			var std: String
+			var etd: String
+			var isCancelled: Bool
+		}
+	}
+
+	func gotDelays(data: Delays?, err: Error?) {
+		dataItems = []
+		guard let data = data else {
+			completion?(self, dataItems, err)
+			return
+		}
+
+		if data.delayedTrains.count == 0 {
+			dataItems.append(DataItem("\(targetCrs) trains: Good Service"))
+		}
+
+		for delay in data.delayedTrains {
+			// If we don't force a line break here, UILabel breaks the line after the "to"
+			// which makes the resulting text look a bit unbalanced. But it only does this
+			// when using the Amiga Forever font in non-editing mode (!?)
+			var text = "\(delay.std) to \(targetCrs):\u{2028}"
+			if delay.isCancelled {
+				text += "Cancelled"
+			} else {
+				let df = DateFormatter()
+				df.dateFormat = "HH:mm"
+				let std = df.date(from: delay.std)
+				let etd = df.date(from: delay.etd)
+				if (std != nil && etd != nil) {
+					let mins = Int(etd!.timeIntervalSince(std!) / 60)
+					text += "\(mins) mins late"
+				} else {
+					text += "Delayed"
+				}
+			}
+			dataItems.append(DataItem(text, flags: [DataItemFlag.warning]))
+		}
+		completion?(self, dataItems, err)
+	}
+}
+
