9d7642ed5a955813c517bcdce9feb2912b3aaf3e
Jason Barrie Morley <jason.morley@inseven.co.uk>
2018-10-16 03:43:57 -0700

    Add rudimentary test for the service.

---
+++ service/tests/test_api.py
@@ -0,0 +1,27 @@
+import os
+import pytest
+import shutil
+import sys
+import tempfile
+
+TESTS_DIR = os.path.dirname(os.path.abspath(__file__))
+SERVICE_DIR = os.path.dirname(TESTS_DIR)
+
+sys.path.append(SERVICE_DIR)
+
+import app
+
+
+@pytest.fixture
+def client():
+    app.app.config['UPLOADS_DIRECTORY'] = tempfile.mkdtemp()
+    app.app.config['TESTING'] = True  # Disable error caching during request handling
+    client = app.app.test_client()
+    yield client
+    shutil.rmtree(app.app.config['UPLOADS_DIRECTORY'])
+
+
+def test_index(client):
+    response = client.get('/')
+    print(response)
+    assert(response.status_code == 200, "Successfully index fetch")
