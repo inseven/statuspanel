b46815e49c1859c456117c74e4f5d5b4ac284ba8
Tom Sutcliffe <tomsci@me.com>
2018-10-16 18:12:47 +0100

    Fixed the last modified caching.

--- nodemcu/panel.lua
+++ nodemcu/panel.lua
@@ -347,18 +347,17 @@ end
 
 statusTable = {}
 
-function getImg()
-	local lastModified
+function getImg(completion)
+	local currentLastModified
 	lastModifiedFile = file.open("last_modified", "r")
 	if lastModifiedFile then
-		lastModified = lastModifiedFile:read(32)
+		currentLastModified = lastModifiedFile:read(32)
 		lastModifiedFile:close()
 	end
 
 	local ifModifiedHeader = ""
-	if lastModified then
-		ifModifiedHeader = "\r\nIf-Modified-Since: "..lastModified
-		lastModified = nil
+	if currentLastModified then
+		ifModifiedHeader = "\r\nIf-Modified-Since: "..currentLastModified
 	end
 	statusTable = {}
 	local req = "GET /api/v1 HTTP/1.1\r\nHost: calendar-image-server.herokuapp.com\r\nConnection: close"..ifModifiedHeader.."\r\n\r\n"
@@ -369,14 +368,15 @@ function getImg()
 	end)
 	local status
 	local bytesRead = 0
-	local lastModified
+	local lastModifiedHeader
 	local f
 	conn:on("receive", function(sock, data)
 		-- print("Got ", #data)
 		if not status then
 			status = tonumber(data:match("^HTTP/1.1 (%d+)"))
-			if status ~= 200 then
-				print("Error fetching image", status)
+			if status == 304 then
+				print("Image not modified since "..currentLastModified)
+			elseif status ~= 200 then
 				addStatus("Error %d returned from server", status)
 			end
 		end
@@ -389,8 +389,8 @@ function getImg()
 
 		if bytesRead == 0 then
 			-- Initial data is the HTTP headers
-			if not lastModified then
-				lastModified = data:match("Last%-Modified: (.-)[\r\n]")
+			if not lastModifiedHeader then
+				lastModifiedHeader = data:match("Last%-Modified: (.-)[\r\n]")
 			end
 			-- Strip remaining headers
 			local _, pos = data:find("\r\n\r\n", 1, true)
@@ -410,14 +410,19 @@ function getImg()
 		if f then
 			f:close()
 		end
-		if lastModified then
+		if lastModifiedHeader and lastModifiedHeader ~= currentLastModified then
 			local lastModifiedFile = assert(file.open("last_modified", "w"))
-			lastModifiedFile:write(lastModified)
+			lastModifiedFile:write(lastModifiedHeader)
 			lastModifiedFile:close()
 		end
 		if status == 200 then
-			print(string.format("Got %d bytes written at %s", bytesRead, lastModified or ""))
-			addStatus("Last updated: %s", lastModified or "?")
+			print(string.format("Got %d bytes written at %s", bytesRead, lastModifiedHeader or ""))
+			addStatus("Last updated: %s", lastModifiedHeader or "?")
+		elseif status == 304 then
+			addStatus("Last updated: %s", currentLastModified)
+		end
+		if completion then
+			node.task.post(completion)
 		end
 	end)
 	local _
