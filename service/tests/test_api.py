284d5d4d69f3b12c73fa845860cbe2d0815ef158
Jason Barrie Morley <jason.morley@inseven.co.uk>
2018-10-16 05:08:16 -0700

    Unit tests for the v1 API.

--- service/tests/test_api.py
+++ service/tests/test_api.py
@@ -6,6 +6,8 @@ import tempfile
 
 TESTS_DIR = os.path.dirname(os.path.abspath(__file__))
 SERVICE_DIR = os.path.dirname(TESTS_DIR)
+ROOT_DIR = os.path.dirname(SERVICE_DIR)
+IMAGES_DIR = os.path.join(ROOT_DIR, "images")
 
 sys.path.append(SERVICE_DIR)
 
@@ -23,10 +25,25 @@ def client():
 
 def test_index(client):
     response = client.get('/')
-    assert(response.status_code == 200, "Fetching index succeeds")
+    assert response.status_code == 200, "Fetching index succeeds"
 
 
 def test_api_v1_get_empty(client):
     response = client.get('/api/v1')
-    print(response.status_code)
-    assert(response.status_code == 200, "Fetching missing upload fails correctly")
+    assert response.status_code == 404, "Fetching missing upload fails correctly"
+
+
+def test_api_v1_put_get_success(client):
+    image_path = os.path.join(IMAGES_DIR, "paisley.png")
+    response = client.post('/api/v1',
+                           content_type="multipart/form-data",
+                           buffered=True,
+                           follow_redirects=True,
+                           data={
+                               'file': (open(image_path, 'rb'), 'paisley.png')
+                           })
+    assert response.status_code == 200, "Upload succeeds"
+    response = client.get('/api/v1')
+    assert response.status_code == 200, "Getting the uploaded file succeeds"
+    with open(image_path, 'rb') as fh:
+        assert response.data == fh.read(), "Downloaded file matches uploaded file"
