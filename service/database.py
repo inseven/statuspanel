7f841bdf2995b3b31f2a8efe15fbc2fe8e372640
Jason Barrie Morley <jason.morley@inseven.co.uk>
2018-10-20 08:33:52 -0700

    Switching to psycopg and implementing migrations

--- service/database.py
+++ service/database.py
@@ -6,37 +6,66 @@ import psycopg2
 
 class Metadata(object):
     SCHEMA_VERSION = "schema_version"
-    
-    
-def create_database(database):
-    logging.info("Creating database...")
+
+
+def empty_migration(cursor):
+    logging.error("Running empty migration...")
+
+
+class Transaction(object):
+
+    def __init__(self, connection):
+        self.connection = connection
+
+    def __enter__(self):
+        self.cursor = self.connection.cursor()
+        return self.cursor
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if exc_type is None and exc_val is None and exc_tb is None:
+            self.connection.commit()
+        else:
+            self.connection.rollback()
+        self.cursor.close()
 
 
 class Database(object):
 
     SCHEMA_VERSION = 1
-    
+
+    # TODO: Ensure the migration structure is correct.
     MIGRATIONS = {
-        1: create_database
+        1: empty_migration
     }
 
     def __init__(self):
-        self.db = postgres.Postgres(os.environ['DATABASE_URL'])
-        
+        self.connection = psycopg2.connect(os.environ['DATABASE_URL'])
+
         # Create the metadata table (used for versioning).
-        self.db.run("CREATE TABLE IF NOT EXISTS metadata (key TEXT NOT NULL, value INT, UNIQUE(key))")
+        with Transaction(self.connection) as cursor:
+            cursor.execute("CREATE TABLE IF NOT EXISTS metadata (key TEXT NOT NULL, value INT, UNIQUE(key))")
 
         # Create the initial version if necessary.
-        # If this statement fails, we can safely assume that there's already a version
-        # present in the database.
         try:
-            logging.info("Attempting to set initial schema_version...")
-            self.set_metadata(Metadata.SCHEMA_VERSION, 0)
+            with Transaction(self.connection) as cursor:
+                cursor.execute("INSERT INTO metadata VALUES (%s, %s)",
+                               (Metadata.SCHEMA_VERSION, 0))
         except psycopg2.IntegrityError:
-            logging.info("schema_version key already exists")
-        
-    def set_metadata(self, key, value):
-        self.db.run("INSERT INTO metadata VALUES (%(key)s, %(value)s)", {"key": key, "value": value})
+            logging.error("schema_version key already exists")
+
+        # Perform a migration if necessary.
+        with Transaction(self.connection) as cursor:
+            cursor.execute("SELECT value FROM metadata WHERE key=%s",
+                           (Metadata.SCHEMA_VERSION, ))
+            result = cursor.fetchone()
+            schema_version = result[0]
+            logging.error(f"Current schema at version {schema_version}")
+            for i in range(schema_version + 1, self.SCHEMA_VERSION + 1):
+                logging.error(f"Performing migration to version {i}...")
+                self.MIGRATIONS[i](cursor)
+            cursor.execute("UPDATE metadata SET value=%s WHERE key=%s",
+                           (self.SCHEMA_VERSION, Metadata.SCHEMA_VERSION))
+            logging.error(f"Updated schema to version {self.SCHEMA_VERSION}")
 
     def migrate(self):
         pass
