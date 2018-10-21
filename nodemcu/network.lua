e98d9af54f174319b851a1831946d6bb4bd825d3
Tom Sutcliffe <tomsci@me.com>
2018-10-21 13:38:26 +0100

    Added QR code rendering and libsodium key generation
    Also cleaned up text rendering APIs

--- nodemcu/network.lua
+++ nodemcu/network.lua
@@ -12,7 +12,9 @@ function getImg(completion)
 	if currentLastModified then
 		ifModifiedHeader = "\r\nIf-Modified-Since: "..currentLastModified
 	end
+	local deviceId = getDeviceId()
 	statusTable = {}
+	addStatus("Device ID: %s", deviceId)
 	local req = "GET /api/v1 HTTP/1.1\r\nHost: calendar-image-server.herokuapp.com\r\nConnection: close"..ifModifiedHeader.."\r\n\r\n"
 	local conn = net.createConnection(net.TCP, 0)
 	conn:on("connection", function(sock)
@@ -29,12 +31,14 @@ function getImg(completion)
 			status = tonumber(data:match("^HTTP/1.1 (%d+)"))
 			if status == 304 then
 				print("Image not modified since "..currentLastModified)
+			elseif status == 404 then
+				print("No image on server yet")
 			elseif status ~= 200 then
 				addStatus("Error %d returned from server", status)
 			end
 		end
 		if status ~= 200 then
-			if status ~= 304 then
+			if status ~= 304 and status ~= 404 then
 				print(data) -- Is probably detailed error description
 			end
 			return
@@ -76,7 +80,7 @@ function getImg(completion)
 		end
 		setStatusLed(0)
 		if completion then
-			node.task.post(completion)
+			node.task.post(function() completion(status) end)
 		end
 	end)
 	if not gw and wifi.sta.getip then
@@ -91,5 +95,104 @@ function getImg(completion)
 		conn:connect(80, dest)
 	else
 		addStatus("No internet connection!")
+		node.task.post(function() completion(nil) end)
 	end
+end
+
+-- Avoid o, i, 0, 1
+local idchars = "abcdefghjklmnpqrstuvwxyz23456789"
+function makeDeviceId()
+	local t = {}
+	for i = 1, 8 do
+		local idx = sodium.randombytes_uniform(#idchars) + 1
+		t[i] = idchars:sub(idx, idx)
+	end
+	return table.concat(t)
+end
+
+local id = nil
+function getDeviceId()
+	if id then
+		return id
+	end
+	local f = file.open("deviceid", "r")
+	if f then
+		id = assert(f:read(8))
+		f:close()
+		return id
+	end
+	-- Otherwise generate one and save it
+	id = makeDeviceId()
+	f = assert(file.open("deviceid", "w"))
+	f:write(id)
+	f:close()
+	return id
+end
+
+function generateKeyPair()
+	local pk, sk = sodium.crypto_box_keypair()
+	local f = assert(file.open("pk", "w"))
+	f:write(pk)
+	f:close()
+	f = assert(file.open("sk", "w"))
+	f:write(sk)
+	f:close()
+	return pk, sk
+end
+
+function getPublicKey()
+	if not file.exists("pk") or not file.exists("sk") then
+		local pk, sk = generateKeyPair()
+		return pk
+	end
+	local f = assert(file.open("pk", "r"))
+	local pk = f:read()
+	f:close()
+	return pk
+end
+
+function getSecretKey()
+	if not file.exists("pk") or not file.exists("sk") then
+		local pk, sk = generateKeyPair()
+		return sk
+	end
+	local f = assert(file.open("sk", "r"))
+	local sk = f:read()
+	f:close()
+	return sk
+end
+
+function getQRCodeURL()
+	local id = getDeviceId()
+	local pk = getPublicKey()
+	-- toBase64 doesn't URL-encode the unsafe chars, so do that too
+	local pkstr = encoder.toBase64(pk):gsub("[/%+%=]", function(ch) return string.format("%%%02X", ch:byte()) end)
+	return string.format("statuspanel:r?id=%s&pk=%s", id, pkstr)
+end
+
+function displayRegisterScreen()
+	local font = require("font")
+	local url = getQRCodeURL()
+	local urlWidth = #url * font.charw
+	local data = qrcodegen.encodeText(url, 1, 5)
+	local sz = qrcodegen.getSize(data)
+	local scale = 8
+	local startx = math.floor((w - sz * scale) / 2)
+	local starty = math.floor((h - sz * scale) / 2)
+	local textPixel = getTextPixelFn(url)
+	local texty = 20
+	local textStart = math.floor((w - urlWidth) / 2)
+	local function getPixel(x, y)
+		if y >= texty and y < texty + font.charh then
+			return textPixel(x - textStart, y - texty)
+		end
+		local codex = math.floor((x - startx) / scale)
+		local codey = math.floor((y - starty) / scale)
+		if codex >= 0 and codex < sz and codey >= 0 and codey < sz then
+			return qrcodegen.getPixel(data, codex, codey) and BLACK or WHITE
+		else
+			return WHITE
+		end
+	end
+	display(getPixel)
 end
