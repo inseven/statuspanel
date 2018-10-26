1687c7b78653984585b0fbf66db875c309f5f05e
Jason Barrie Morley <jason.morley@inseven.co.uk>
2018-10-25 23:52:00 -0700

    Update the smoke tests.

--- service/tests/smoke/test_smoke.py
+++ service/tests/smoke/test_smoke.py
@@ -1,9 +1,10 @@
+import io
 import os
 import pytest
+import requests
 import shutil
 import sys
 import tempfile
-import requests
 import urllib
 
 TESTS_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -37,24 +38,18 @@ def test_index(client):
     assert response.status_code == 200, "Fetching index succeeds"
 
 
-# def test_api_v1_get_empty(client):
-#     response = client.get('/api/v1')
-#     assert response.status_code == 404, "Fetching missing upload fails"
-
-
-def upload(client, url, path):
+def upload(client, url, data):
     image_path = os.path.join(IMAGES_DIR, "paisley.png")
-    return client.post(url, files={'file': open(path, 'rb')})
+    return client.post(url, files={'file': io.BytesIO(data)})
 
 
 def test_api_v1_put_get_success(client):
-    image_path = os.path.join(IMAGES_DIR, "paisley.png")
-    response = upload(client, '/api/v1', image_path)
+    data = os.urandom(307200)
+    response = upload(client, '/api/v1', data)
     assert response.status_code == 200, "Upload succeeds"
     response = client.get('/api/v1')
     assert response.status_code == 200, "Getting the uploaded file succeeds"
-    with open(image_path, 'rb') as fh:
-        assert response.content == fh.read(), "Downloaded file matches uploaded file"
+    assert response.content == data, "Downloaded file matches uploaded file"
 
 
 def test_api_v2_get_no_identifier(client):
@@ -73,25 +68,24 @@ def test_api_v2_get_invalid_identifier(client):
 
 
 def test_api_v2_put_invalid_identifier_fails(client):
-    image_path = os.path.join(IMAGES_DIR, "paisley.png")
-    response = upload(client, '/api/v2/bad', image_path)
+    data = os.urandom(307200)
+    response = upload(client, '/api/v2/bad', data)
     assert response.status_code == 400, "Upload fails"
 
 
 def test_api_v2_put_get_success(client):
     url = '/api/v2/abcdefgh'
-    image_path = os.path.join(IMAGES_DIR, "paisley.png")
-    response = upload(client, url, image_path)
+    data = os.urandom(307200)
+    response = upload(client, url, data)
     assert response.status_code == 200, "Upload succeeds"
     response = client.get(url)
     assert response.status_code == 200, "Getting the uploaded file succeeds"
-    with open(image_path, 'rb') as fh:
-        assert response.content == fh.read(), "Downloaded file matches uploaded file"
+    assert response.content == data, "Downloaded file matches uploaded file"
 
 
 def test_api_v2_put_get_different_identifiers_fails(client):
-    image_path = os.path.join(IMAGES_DIR, "paisley.png")
-    response = upload(client, '/api/v2/abcdefgh', image_path)
+    data = os.urandom(307200)
+    response = upload(client, '/api/v2/abcdefgh', data)
     assert response.status_code == 200, "Upload succeeds"
     response = client.get('/api/v2/01234567')
     assert response.status_code == 404, "Getting the uploaded file succeeds"
@@ -100,20 +94,37 @@ def test_api_v2_put_get_different_identifiers_fails(client):
 def test_api_v2_put_get_multiple_success(client):
     url_1 = '/api/v2/abcdefgh'
     url_2 = '/api/v2/bcdefghi'
-    image_path_1 = os.path.join(IMAGES_DIR, "paisley.png")
-    image_path_2 = os.path.join(IMAGES_DIR, "red.png")
+    data_1 = os.urandom(307200)
+    data_2 = os.urandom(307200)
 
-    response = upload(client, url_1, image_path_1)
+    response = upload(client, url_1, data_1)
     assert response.status_code == 200, "Upload succeeds"
-    response = upload(client, url_2, image_path_2)
+    response = upload(client, url_2, data_2)
     assert response.status_code == 200, "Upload succeeds"
 
     response = client.get(url_1)
     assert response.status_code == 200, "Getting the uploaded file succeeds"
-    with open(image_path_1, 'rb') as fh:
-        assert response.content == fh.read(), "Downloaded file matches uploaded file"
+    assert response.content == data_1, "Downloaded file matches uploaded file"
 
     response = client.get(url_2)
     assert response.status_code == 200, "Getting the uploaded file succeeds"
-    with open(image_path_2, 'rb') as fh:
-        assert response.content == fh.read(), "Downloaded file matches uploaded file"
+    assert response.content == data_2, "Downloaded file matches uploaded file"
+
+
+def test_api_v2_put_get_multiple_same_identifier_success(client):
+    url = '/api/v2/abcdefgh'
+    data_1 = os.urandom(207234)
+    data_2 = os.urandom(307200)
+
+    response = upload(client, url, data_1)
+    assert response.status_code == 200, "Upload succeeds"
+    response = upload(client, url, data_2)
+    assert response.status_code == 200, "Upload succeeds"
+
+    response = client.get(url)
+    assert response.status_code == 200, "Getting the uploaded file succeeds"
+    assert response.content != data_1, "Downloaded file matches uploaded file"
+
+    response = client.get(url)
+    assert response.status_code == 200, "Getting the uploaded file succeeds"
+    assert response.content == data_2, "Downloaded file matches uploaded file"
