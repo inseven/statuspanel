21bf2792f67ade15a2bc30a4fa40f437acf54d73
Jason Barrie Morley <jason.morley@inseven.co.uk>
2018-10-29 22:04:09 -0700

    Seeing if a stream read stops H18 errors in Heroku.

--- service/app.py
+++ service/app.py
@@ -47,6 +47,7 @@ def check_identifier(fn):
     def inner(*args, **kwargs):
         logging.info(f"Checking identifier '{kwargs['identifier']}'...")
         if not re.match(r"^[0-9a-z]{8}$", kwargs['identifier']):
+            request.stream.read()
             return f"Invalid identifier '{kwargs['identifier']}'", 400
         return fn(*args, **kwargs)
     return inner
