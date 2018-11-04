7ba832be8b31438f08bd6ffa263704e75ecd2872
Tom Sutcliffe <tomsci@me.com>
2018-11-04 19:46:50 +0000

    Converted all .swift files to use spaces not tabs
    /me bows to the inevitable

--- StatusPanel/JSONRequest.swift
+++ StatusPanel/JSONRequest.swift
@@ -10,32 +10,32 @@ import Foundation
 
 class JSONRequest {
 
-	static func makeRequest<T>(url: URL, session: URLSession? = nil, onCompletion: @escaping (T?, Error?) -> Void) where T : Decodable {
-		let session = session ?? URLSession.shared
-		let task = session.dataTask(with: url) { (data: Data?, response: URLResponse?, err: Error?) in
-			if let err = err {
-				print("Error fetching \(url): \(err)")
-				onCompletion(nil, err)
-				return
-			}
-			guard let httpResponse = response as? HTTPURLResponse,
-				httpResponse.statusCode == 200,
-				let data = data
-				else {
-					print("Server errored! resp = \(response!)")
-					let err = NSError(domain: NSURLErrorDomain, code: URLError.badServerResponse.rawValue, userInfo: ["response": response!])
-					onCompletion(nil, err)
-					return
-			}
-			do {
-				let obj = try JSONDecoder().decode(T.self, from: data)
-				onCompletion(obj, nil)
-			} catch {
-				print("Failed to decode obj from \(data)")
-				onCompletion(nil, error)
-			}
-		}
-		task.resume()
-	}
+    static func makeRequest<T>(url: URL, session: URLSession? = nil, onCompletion: @escaping (T?, Error?) -> Void) where T : Decodable {
+        let session = session ?? URLSession.shared
+        let task = session.dataTask(with: url) { (data: Data?, response: URLResponse?, err: Error?) in
+            if let err = err {
+                print("Error fetching \(url): \(err)")
+                onCompletion(nil, err)
+                return
+            }
+            guard let httpResponse = response as? HTTPURLResponse,
+                httpResponse.statusCode == 200,
+                let data = data
+                else {
+                    print("Server errored! resp = \(response!)")
+                    let err = NSError(domain: NSURLErrorDomain, code: URLError.badServerResponse.rawValue, userInfo: ["response": response!])
+                    onCompletion(nil, err)
+                    return
+            }
+            do {
+                let obj = try JSONDecoder().decode(T.self, from: data)
+                onCompletion(obj, nil)
+            } catch {
+                print("Failed to decode obj from \(data)")
+                onCompletion(nil, error)
+            }
+        }
+        task.resume()
+    }
 }
 
