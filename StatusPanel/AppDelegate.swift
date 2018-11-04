80845ab1fb753f98bb16027af94923e722304ea1
Tom Sutcliffe <tomsci@me.com>
2018-11-04 20:32:09 +0000

    Integrated PR #1 data now refreshed when app brought to foreground

--- StatusPanel/AppDelegate.swift
+++ StatusPanel/AppDelegate.swift
@@ -34,6 +34,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        sourceController.fetch()
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
@@ -73,4 +74,3 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
 }
-
