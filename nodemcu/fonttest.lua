f00c5e1b66b0a7919dfd527b92faedf5ef275780
Tom Sutcliffe <tomsci@me.com>
2018-11-05 19:53:02 +0000

    Spaces II: The Respacening

--- nodemcu/fonttest.lua
+++ nodemcu/fonttest.lua
@@ -2,9 +2,9 @@
 
 -- Neither standard Lua 5.1 nor 5.3 have a bit library exactly matching what NodeMCU code expects
 bit = {
-	band = function(a, b) return a & b end,
-	rshift = function(a, b) return a >> b end,
-	lshift = function(a, b) return a << b end,
+    band = function(a, b) return a & b end,
+    rshift = function(a, b) return a >> b end,
+    lshift = function(a, b) return a << b end,
 }
 
 font = require "font"
@@ -12,12 +12,12 @@ font = require "font"
 text = "Testing 123!"
 
 for y = 0, font.charh-1 do
-	local bits = {}
-	for x = 0, (font.charw * #text)-1 do
-		local textPos = 1 + (x // font.charw)
-		local char = text:sub(textPos, textPos)
-		local chx = x % font.charw
-		table.insert(bits, font.getPixel(char, chx, y) and "X" or " ")
-	end
-	print(table.concat(bits))
+    local bits = {}
+    for x = 0, (font.charw * #text)-1 do
+        local textPos = 1 + (x // font.charw)
+        local char = text:sub(textPos, textPos)
+        local chx = x % font.charw
+        table.insert(bits, font.getPixel(char, chx, y) and "X" or " ")
+    end
+    print(table.concat(bits))
 end
--- nodemcu/panel.lua
+++ nodemcu/panel.lua
@@ -53,97 +53,97 @@ local Busy, Reset, DC, CS, Sck, Mso, SpiId = Busy, Reset, DC, CS, Sck, Mso, SpiI
 
 local sendByte
 if esp32 then
-	function sendByte(b, dc)
-		gpio_write(DC, dc)
-		spidevice_transfer(spidevice, ch(b))
-		bytesSent = bytesSent + 1
-	end
+    function sendByte(b, dc)
+        gpio_write(DC, dc)
+        spidevice_transfer(spidevice, ch(b))
+        bytesSent = bytesSent + 1
+    end
 else
-	function sendByte(b, dc)
-		gpio_write(DC, dc)
-		-- Have to toggle CS on *every byte*
-		gpio_write(CS, 0)
-		spi.send(SpiId, b)
-		bytesSent = bytesSent + 1
-		gpio_write(CS, 1)
-	end
+    function sendByte(b, dc)
+        gpio_write(DC, dc)
+        -- Have to toggle CS on *every byte*
+        gpio_write(CS, 0)
+        spi.send(SpiId, b)
+        bytesSent = bytesSent + 1
+        gpio_write(CS, 1)
+    end
 end
 
 local function cmd(id, ...)
-	assert(id, "Bad ID??")
-	sendByte(id, DC_CMD)
-	local nargs = select("#", ...)
-	if nargs > 0 then
-		local data = { ... }
-		for i = 1, nargs do
-			sendByte(data[i], DC_DATA)
-		end
-	end
+    assert(id, "Bad ID??")
+    sendByte(id, DC_CMD)
+    local nargs = select("#", ...)
+    if nargs > 0 then
+        local data = { ... }
+        for i = 1, nargs do
+            sendByte(data[i], DC_DATA)
+        end
+    end
 end
 
 local function doneFn()
-	print("Done!")
+    print("Done!")
 end
 
 function initp(completion)
-	if not completion then completion = doneFn end
-	reset(function()
-		cmd(POWER_SETTING, 0x37, 0x00)
-		cmd(PANEL_SETTING, 0xCF, 0x08)
-		cmd(BOOSTER_SOFT_START, 0xC7, 0xCC, 0x28)
-		cmd(POWER_ON)
-		waitUntilIdle(function()
-			print("Powered on")
-			cmd(PLL_CONTROL, 0x3C)
-			cmd(TEMPERATURE_CALIBRATION, 0x00)
-			cmd(VCOM_AND_DATA_INTERVAL_SETTING, 0x77)
-			cmd(TCON_SETTING, 0x22)
-			cmd(TCON_RESOLUTION, 0x02, 0x80, 0x01, 0x80) -- 0x0280 = 640, 0x0180 = 384
-			cmd(VCM_DC_SETTING, 0x1E)
-			cmd(0xE5, 0x03) -- FLASH MODE
-			completion()
-		end)
-	end)
+    if not completion then completion = doneFn end
+    reset(function()
+        cmd(POWER_SETTING, 0x37, 0x00)
+        cmd(PANEL_SETTING, 0xCF, 0x08)
+        cmd(BOOSTER_SOFT_START, 0xC7, 0xCC, 0x28)
+        cmd(POWER_ON)
+        waitUntilIdle(function()
+            print("Powered on")
+            cmd(PLL_CONTROL, 0x3C)
+            cmd(TEMPERATURE_CALIBRATION, 0x00)
+            cmd(VCOM_AND_DATA_INTERVAL_SETTING, 0x77)
+            cmd(TCON_SETTING, 0x22)
+            cmd(TCON_RESOLUTION, 0x02, 0x80, 0x01, 0x80) -- 0x0280 = 640, 0x0180 = 384
+            cmd(VCM_DC_SETTING, 0x1E)
+            cmd(0xE5, 0x03) -- FLASH MODE
+            completion()
+        end)
+    end)
 end
 
 function reset(completion)
-	if not completion then completion = doneFn end
-	gpio.write(Reset, 0)
-	tmr.create():alarm(200, tmr.ALARM_SINGLE, function()
-		gpio.write(Reset, 1)
-		tmr.create():alarm(200, tmr.ALARM_SINGLE, function()
-			node.task.post(completion)
-		end)
-	end)
+    if not completion then completion = doneFn end
+    gpio.write(Reset, 0)
+    tmr.create():alarm(200, tmr.ALARM_SINGLE, function()
+        gpio.write(Reset, 1)
+        tmr.create():alarm(200, tmr.ALARM_SINGLE, function()
+            node.task.post(completion)
+        end)
+    end)
 end
 
 function waitUntilIdle(completion)
-	if not completion then completion = doneFn end
-	if gpio.read(Busy) == 1 then
-		-- Already idle
-		node.task.post(completion)
-		return
-	end
-	print("WaitUntilIdle...")
+    if not completion then completion = doneFn end
+    if gpio.read(Busy) == 1 then
+        -- Already idle
+        node.task.post(completion)
+        return
+    end
+    print("WaitUntilIdle...")
 
-	tmr.create():alarm(100, tmr.ALARM_SEMI, function(t)
-		if gpio.read(Busy) == 0 then
-			t:start()
-		else
-			print("Wait complete!")
-			t:unregister()
-			node.task.post(completion)
-		end
-	end)
+    tmr.create():alarm(100, tmr.ALARM_SEMI, function(t)
+        if gpio.read(Busy) == 0 then
+            t:start()
+        else
+            print("Wait complete!")
+            t:unregister()
+            node.task.post(completion)
+        end
+    end)
 end
 
 function sleep(completion)
-	if not completion then completion = doneFn end
-	cmd(POWER_OFF)
-	waitUntilIdle(function()
-		cmd(DEEP_SLEEP, 0xA5)
-		node.task.post(completion)
-	end)
+    if not completion then completion = doneFn end
+    cmd(POWER_OFF)
+    waitUntilIdle(function()
+        cmd(DEEP_SLEEP, 0xA5)
+        node.task.post(completion)
+    end)
 end
 
 WHITE = 3
@@ -157,146 +157,146 @@ w, h = 640, 384
 -- The actual display code!
 
 function display(getPixelFn, completion)
-	setStatusLed(1)
-	if not getPixelFn then
-		getPixelFn = whiteColourBlack
-	end
-	local t = uptime()
-	cmd(DATA_START_TRANSMISSION_1)
-	local y = 0
-	bytesSent = 0
-	if esp32 then
-		gpio_write(DC, DC_DATA)
-	end
-	local function drawLine()
-		if y == h then
-			local pixels = bytesSent * 2
-			cmd(DISPLAY_REFRESH)
-			waitUntilIdle(function()
-				local elapsed = (uptime() - t) / 1000000
-				sleep(function()
-					print(string.format("Wrote %d pixels, took %ds", pixels, elapsed))
-					setStatusLed(0)
-					if completion then
-						completion()
-					end
-				end)
-			end)
-			return
-		end
+    setStatusLed(1)
+    if not getPixelFn then
+        getPixelFn = whiteColourBlack
+    end
+    local t = uptime()
+    cmd(DATA_START_TRANSMISSION_1)
+    local y = 0
+    bytesSent = 0
+    if esp32 then
+        gpio_write(DC, DC_DATA)
+    end
+    local function drawLine()
+        if y == h then
+            local pixels = bytesSent * 2
+            cmd(DISPLAY_REFRESH)
+            waitUntilIdle(function()
+                local elapsed = (uptime() - t) / 1000000
+                sleep(function()
+                    print(string.format("Wrote %d pixels, took %ds", pixels, elapsed))
+                    setStatusLed(0)
+                    if completion then
+                        completion()
+                    end
+                end)
+            end)
+            return
+        end
 
-		-- print("Line " ..tostring(y))
-		setStatusLed(y % 2)
-		if esp32 then
-			local line = {}
-			for i = 0, (w / 2) - 1 do
-				local x = i * 2
-				local b = getPixelFn(x, y) * 16 + getPixelFn(x + 1, y)
-				line[i+1] = ch(b)
-			end
-			local data = table.concat(line)
-			spidevice_transfer(spidevice, data)
-			bytesSent = bytesSent + #data
-		else
-			for x = 0, w - 1, 2 do
-				local b = getPixelFn(x, y) * 16 + getPixelFn(x + 1, y)
-				sendByte(b, DC_DATA)
-			end
-		end
-		y = y + 1
-		node.task.post(drawLine)
-		-- tmr.create():alarm(10, tmr.ALARM_SINGLE, drawLine)
-	end
-	node.task.post(drawLine)
+        -- print("Line " ..tostring(y))
+        setStatusLed(y % 2)
+        if esp32 then
+            local line = {}
+            for i = 0, (w / 2) - 1 do
+                local x = i * 2
+                local b = getPixelFn(x, y) * 16 + getPixelFn(x + 1, y)
+                line[i+1] = ch(b)
+            end
+            local data = table.concat(line)
+            spidevice_transfer(spidevice, data)
+            bytesSent = bytesSent + #data
+        else
+            for x = 0, w - 1, 2 do
+                local b = getPixelFn(x, y) * 16 + getPixelFn(x + 1, y)
+                sendByte(b, DC_DATA)
+            end
+        end
+        y = y + 1
+        node.task.post(drawLine)
+        -- tmr.create():alarm(10, tmr.ALARM_SINGLE, drawLine)
+    end
+    node.task.post(drawLine)
 end
 
 local mod3 = { [0] = WHITE, [1] = COLOURED, [2] = BLACK }
 function whiteColourBlack(x, y)
-	-- Draw alternate lines of white, colour and black
-	return mod3[y % 3]
+    -- Draw alternate lines of white, colour and black
+    return mod3[y % 3]
 end
 
 function white()
-	display(function() return WHITE end)
+    display(function() return WHITE end)
 end
 
 function black()
-	display(function() return BLACK end)
+    display(function() return BLACK end)
 end
 
 function dither()
-	display(function(x, y)
-		if x % 2 == 0 or y % 2 == 0 then
-			return COLOURED
-		else
-			return BLACK
-		end
-	end)
-end	 
+    display(function(x, y)
+        if x % 2 == 0 or y % 2 == 0 then
+            return COLOURED
+        else
+            return BLACK
+        end
+    end)
+end  
 
 function displayImg(completion)
-	local rle = require("rle")
-	local f = assert(file.open("img_panel_rle", "rb"))
-	local function reader()
-		local ch = f:read(1)
-		if ch then
-			return string.byte(ch)
-		else
-			return nil
-		end
-	end
-	local ctx = rle.beginDecode(reader)
-	local statusLineStart, getTextPixel
-	if esp32 then
-		-- Not enough RAM for this on esp8266 (try lcross?)
-		statusLineStart = h - require("font").charh
-		local statusText = table.concat(statusTable, " | ")
-		local fg = BLACK
-		local bg
-		if statusText:match("^!") then
-			bg = COLOURED
-		else
-			bg = WHITE
-		end
-		getTextPixel = getTextPixelFn(statusText, fg, bg)
-	end
+    local rle = require("rle")
+    local f = assert(file.open("img_panel_rle", "rb"))
+    local function reader()
+        local ch = f:read(1)
+        if ch then
+            return string.byte(ch)
+        else
+            return nil
+        end
+    end
+    local ctx = rle.beginDecode(reader)
+    local statusLineStart, getTextPixel
+    if esp32 then
+        -- Not enough RAM for this on esp8266 (try lcross?)
+        statusLineStart = h - require("font").charh
+        local statusText = table.concat(statusTable, " | ")
+        local fg = BLACK
+        local bg
+        if statusText:match("^!") then
+            bg = COLOURED
+        else
+            bg = WHITE
+        end
+        getTextPixel = getTextPixelFn(statusText, fg, bg)
+    end
 
-	local function getPixel(x, y)
-		if statusLineStart and y >= statusLineStart then
-			return getTextPixel(x, y - statusLineStart)
-		else
-			-- getPixel is always called in sequence, so don't need to seek
-			return rle.getByte(ctx)
-		end
-	end
+    local function getPixel(x, y)
+        if statusLineStart and y >= statusLineStart then
+            return getTextPixel(x, y - statusLineStart)
+        else
+            -- getPixel is always called in sequence, so don't need to seek
+            return rle.getByte(ctx)
+        end
+    end
 
-	display(getPixel, function()
-		f:close()
-		if completion then completion() end
-	end)
+    display(getPixel, function()
+        f:close()
+        if completion then completion() end
+    end)
 end
 
 function getTextPixelFn(text, fg, bg)
-	local font = require("font")
-	local charw, charh = font.charw, font.charh
-	local BLACK, WHITE = BLACK, WHITE
-	if not fg then fg = BLACK end
-	if not bg then bg = WHITE end
-	return function(x, y)
-		if x < 0 or x >= #text * charw or y < 0 or y >= charh then
-			return bg
-		end
-		local textPos = 1 + math.floor(x / charw)
-		local char = text:sub(textPos, textPos)
-		local chx = x % charw
-		return font.getPixel(char, chx, y) and fg or bg
-	end
+    local font = require("font")
+    local charw, charh = font.charw, font.charh
+    local BLACK, WHITE = BLACK, WHITE
+    if not fg then fg = BLACK end
+    if not bg then bg = WHITE end
+    return function(x, y)
+        if x < 0 or x >= #text * charw or y < 0 or y >= charh then
+            return bg
+        end
+        local textPos = 1 + math.floor(x / charw)
+        local char = text:sub(textPos, textPos)
+        local chx = x % charw
+        return font.getPixel(char, chx, y) and fg or bg
+    end
 end
 
 function displayText()
-	local text = "Hello, World!"
-	local oldh = h
-	local getPixel = getTextPixelFn(text)
-	h = charh
-	display(getPixel, function() h = oldh end)
+    local text = "Hello, World!"
+    local oldh = h
+    local getPixel = getTextPixelFn(text)
+    h = charh
+    display(getPixel, function() h = oldh end)
 end
