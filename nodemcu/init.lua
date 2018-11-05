4636f7689d815754bdb4b7d0bdd77b37bb2c270b
Tom Sutcliffe <tomsci@me.com>
2018-11-05 19:53:02 +0000

    Spaces II: The Respacening

--- nodemcu/init.lua
+++ nodemcu/init.lua
@@ -5,125 +5,125 @@
 esp32 = (gpio.mode == nil)
 
 if esp32 then
-	-- No more horrible mappings from SDK pin numbers to GPIO numbers!
-	Busy = 13
-	Reset = 27
-	DC = 33
-	CS = 15
-	Sck = 5 -- I can't find this documented anywhere other than pins_arduino.h...
-	Mosi = 18 -- Ditto
-	SpiId = 1 -- HSPI (doesn't place any restriction on pins)
-	StatusLed = 21
-	AutoPin = 14
-	VBat = 7 -- That is, ADC1_CH7 aka GPIO 35 (internally connected to BAT)
+    -- No more horrible mappings from SDK pin numbers to GPIO numbers!
+    Busy = 13
+    Reset = 27
+    DC = 33
+    CS = 15
+    Sck = 5 -- I can't find this documented anywhere other than pins_arduino.h...
+    Mosi = 18 -- Ditto
+    SpiId = 1 -- HSPI (doesn't place any restriction on pins)
+    StatusLed = 21
+    AutoPin = 14
+    VBat = 7 -- That is, ADC1_CH7 aka GPIO 35 (internally connected to BAT)
 else
-	-- See https://learn.adafruit.com/adafruit-feather-huzzah-esp8266/pinouts
-	Busy = 4 -- GPIO 2
-	Reset = 1 -- GPIO 5
-	DC = 2 -- GPIO 4
-	CS = 0 -- GPIO 16
-	SpiId = 1 -- HSPI (CLK=14, MOSI=13, MISO=12)
-	SpiClockDiv = 40
+    -- See https://learn.adafruit.com/adafruit-feather-huzzah-esp8266/pinouts
+    Busy = 4 -- GPIO 2
+    Reset = 1 -- GPIO 5
+    DC = 2 -- GPIO 4
+    CS = 0 -- GPIO 16
+    SpiId = 1 -- HSPI (CLK=14, MOSI=13, MISO=12)
+    SpiClockDiv = 40
 end
 
 function configurePins()
-	if esp32 then
-		gpio.config({
-			gpio = { Reset, DC, StatusLed },
-			dir = gpio.OUT,
-		}, {
-			gpio = Busy,
-			dir = gpio.IN
-		}, {
-			gpio = AutoPin,
-			dir = gpio.IN,
-			pull = gpio.PULL_DOWN
-		})
-		local spimaster = spi.master(SpiId, {
-			sclk = Sck,
-			mosi = Mosi,
-			max_transfer_sz = 0,
-		}, 1) -- 1 means enable DMA
-		spidevice = spimaster:device({
-			cs = CS,
-			mode = 0,
-			freq = 2*1000*1000, -- ie 2 MHz
-		})
-		adc.setup(adc.ADC1, VBat, adc.ATTEN_11db)
-		adc.setwidth(adc.ADC1, 12)
-	else
-		gpio.mode(Reset, gpio.OUTPUT)
-		gpio.mode(DC, gpio.OUTPUT)
-		gpio.mode(CS, gpio.OUTPUT)
-		gpio.mode(Busy, gpio.INPUT)
-		-- See https://www.waveshare.com/wiki/7.5inch_e-Paper_HAT_(B)#Communication_protocol
-		spi.setup(SpiId, spi.MASTER, spi.CPOL_LOW, spi.CPHA_LOW, 8, SpiClockDiv)
-		gpio.write(CS, 1)
-	end
-	gpio.write(Reset, 0)
+    if esp32 then
+        gpio.config({
+            gpio = { Reset, DC, StatusLed },
+            dir = gpio.OUT,
+        }, {
+            gpio = Busy,
+            dir = gpio.IN
+        }, {
+            gpio = AutoPin,
+            dir = gpio.IN,
+            pull = gpio.PULL_DOWN
+        })
+        local spimaster = spi.master(SpiId, {
+            sclk = Sck,
+            mosi = Mosi,
+            max_transfer_sz = 0,
+        }, 1) -- 1 means enable DMA
+        spidevice = spimaster:device({
+            cs = CS,
+            mode = 0,
+            freq = 2*1000*1000, -- ie 2 MHz
+        })
+        adc.setup(adc.ADC1, VBat, adc.ATTEN_11db)
+        adc.setwidth(adc.ADC1, 12)
+    else
+        gpio.mode(Reset, gpio.OUTPUT)
+        gpio.mode(DC, gpio.OUTPUT)
+        gpio.mode(CS, gpio.OUTPUT)
+        gpio.mode(Busy, gpio.INPUT)
+        -- See https://www.waveshare.com/wiki/7.5inch_e-Paper_HAT_(B)#Communication_protocol
+        spi.setup(SpiId, spi.MASTER, spi.CPOL_LOW, spi.CPHA_LOW, 8, SpiClockDiv)
+        gpio.write(CS, 1)
+    end
+    gpio.write(Reset, 0)
 end
 
 function init()
-	-- First things first, start bringing up WiFi since getting an IP address takes time
-	if esp32 then
-		wifi.mode(wifi.STATION)
-		wifi.sta.on("got_ip", function(name, event)
-			print("Got IP "..event.ip)
-			ip = event.ip
-			gw = event.gw
-			-- Why is the default allocation limit set to 4KB? Why even is there one?
-			node.egc.setmode(node.egc.ON_ALLOC_FAILURE)
+    -- First things first, start bringing up WiFi since getting an IP address takes time
+    if esp32 then
+        wifi.mode(wifi.STATION)
+        wifi.sta.on("got_ip", function(name, event)
+            print("Got IP "..event.ip)
+            ip = event.ip
+            gw = event.gw
+            -- Why is the default allocation limit set to 4KB? Why even is there one?
+            node.egc.setmode(node.egc.ON_ALLOC_FAILURE)
 
-			if gpio.read(AutoPin) == 1 then
-				print("Doing stuff")
-				main()
-			end
-		end)
-		wifi.start()
-		wifi.sta.connect()
-	end
+            if gpio.read(AutoPin) == 1 then
+                print("Doing stuff")
+                main()
+            end
+        end)
+        wifi.start()
+        wifi.sta.connect()
+    end
 
-	configurePins()
+    configurePins()
 
-	-- Finally, pull in other modules
-	require "panel"
-	require "network"
+    -- Finally, pull in other modules
+    require "panel"
+    require "network"
 end
 
 
 statusTable = {}
 
 function setStatusLed(val)
-	if StatusLed then
-		gpio.write(StatusLed, val)
-	end
+    if StatusLed then
+        gpio.write(StatusLed, val)
+    end
 end
 
 function addStatus(...)
-	local status = string.format(...)
-	print(status)
-	table.insert(statusTable, status)
+    local status = string.format(...)
+    print(status)
+    table.insert(statusTable, status)
 end
 
 function getBatteryVoltage()
-	-- At 11db attenuation and 12 bits width, 4095 = VDD_A
-	local val = adc.read(adc.ADC1, VBat)
-	print("Raw ADC val", val)
-	-- In theory result in mV should be (val * 3.3 * 2) / 4.096
-	-- In practice, calibration seems off so we use a bigger number (~3.5)
-	return math.floor((val * 6973) / 4096)
+    -- At 11db attenuation and 12 bits width, 4095 = VDD_A
+    local val = adc.read(adc.ADC1, VBat)
+    print("Raw ADC val", val)
+    -- In theory result in mV should be (val * 3.3 * 2) / 4.096
+    -- In practice, calibration seems off so we use a bigger number (~3.5)
+    return math.floor((val * 6973) / 4096)
 end
 
 function getBatteryVoltageStatus()
-	local val = math.floor(getBatteryVoltage() / 100) -- ie 42 for 4.2V
-	-- Warn below 3.4V?
-	local warn = (val < 34) and "!" or ""
-	local v = math.floor(val / 10)
-	local dv = val - v*10
-	return string.format("%s%d.%dV", warn, v, dv)
+    local val = math.floor(getBatteryVoltage() / 100) -- ie 42 for 4.2V
+    -- Warn below 3.4V?
+    local warn = (val < 34) and "!" or ""
+    local v = math.floor(val / 10)
+    local dv = val - v*10
+    return string.format("%s%d.%dV", warn, v, dv)
 end
 
 local ok, err = pcall(init)
 if not ok then
-	addStatus(err)
+    addStatus(err)
 end
