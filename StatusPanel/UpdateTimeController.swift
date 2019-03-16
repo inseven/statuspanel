cfd5cd99b4ec9a6e80a6065af54810fbe10ecb85
Tom Sutcliffe <tomsci@me.com>
2019-03-16 17:54:09 +0000

    Adding Settings UI for the update time

---
+++ StatusPanel/UpdateTimeController.swift
@@ -0,0 +1,29 @@
+//
+//  UpdateTimeController.swift
+//  StatusPanel
+//
+//  Created by Tom Sutcliffe on 16/03/2019.
+//  Copyright © 2019 Tom Sutcliffe. All rights reserved.
+//
+
+import UIKit
+
+class UpdateTimeController: UIViewController {
+
+    @IBOutlet weak var datePicker: UIDatePicker!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        datePicker.date = Date.init(timeIntervalSinceReferenceDate: Config().updateTime)
+    }
+
+    @IBAction func timeChanged(sender: UIDatePicker, forEvent event: UIEvent) {
+        let newTime = sender.date.timeIntervalSinceReferenceDate
+        Config().updateTime = newTime
+    }
+
+}
