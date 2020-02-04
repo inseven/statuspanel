0ba3f90612fef39702ecb850d50d95bfe1867063
Jason Barrie Morley <jason.morley@inseven.co.uk>
2020-02-04 07:09:55 -0800

    Register the device with the server.

---
+++ StatusPanel/Client.swift
@@ -0,0 +1,44 @@
+//
+//  Client.swift
+//  StatusPanel
+//
+//  Created by Jason Barrie Morley on 04/02/2020.
+//  Copyright © 2020 Tom Sutcliffe. All rights reserved.
+//
+
+import Foundation
+
+class Client {
+
+    let baseUrl: URL
+
+    init(baseUrl: String) {
+        var url = URL.init(string: baseUrl)!
+        url.appendPathComponent("api/v3")
+        self.baseUrl = url
+    }
+
+    func registerDevice(token: Data, completionHandler: @escaping (Bool, Error?) -> Void) {
+        let json: [String: String] = ["token": token.base64EncodedString()]
+        do {
+            var request = URLRequest(url: self.baseUrl.appendingPathComponent("device/"))
+            request.httpMethod = "POST"
+            request.httpBody = try JSONSerialization.data(withJSONObject: json)
+            let task = URLSession.shared.dataTask(with: request) { data, response, error in
+                guard let data = data, error == nil else {
+                    completionHandler(false, error)
+                    return
+                }
+                let responseJSON = try? JSONSerialization.jsonObject(with: data, options: [])
+                if let responseJSON = responseJSON as? [String: Any] {
+                    print(responseJSON)
+                }
+                completionHandler(true, nil)
+            }
+            task.resume()
+        } catch {
+            completionHandler(false, error)
+        }
+    }
+
+}
