7f097a06a1a16c7b7f6f1b046e6a972611bd2add
Jason Barrie Morley <jason.morley@inseven.co.uk>
2020-02-05 04:45:52 -0800

    Rudimentary support for sending an APNS notification.

---
+++ service/apns.py
@@ -0,0 +1,25 @@
+import base64
+import binascii
+import os
+
+import gobiko.apns
+
+
+class APNS(object):
+
+    def __init__(self, use_sandbox=False):
+        self._client = gobiko.apns.APNsClient(
+            team_id="S4WXAUZQEV",
+            bundle_id="uk.co.inseven.status-panel",
+            auth_key_id="V5XKL2D8B9",
+            auth_key=os.environ['JWT_PRIVATE_KEY'],
+            use_sandbox=use_sandbox,
+        )
+
+    def send_keepalive(self, device_tokens):
+        tokens = [binascii.hexlify(base64.b64decode(token)).decode('ascii') for token in device_tokens]
+        self._client.send_bulk_message(
+            tokens,
+            "Amiga Forever",
+            content_available=True,
+        )
