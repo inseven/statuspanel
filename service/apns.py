c8a9f7983b06f01de9bdf9622a439105481b2204
Jason Barrie Morley <jason.morley@inseven.co.uk>
2020-02-09 22:20:03 -0800

    Better support for sending APNS notifications locally (for debugging), and improved APNS error handling.

--- service/apns.py
+++ service/apns.py
@@ -9,20 +9,47 @@ def encode_token(token):
     return binascii.hexlify(base64.b64decode(token)).decode('ascii')
 
 
+class BadTokens(Exception):
+
+    def __init__(self, tokens):
+        super(BadTokens, self).__init__()
+        self.tokens = tokens
+
+
 class APNS(object):
 
     def __init__(self, use_sandbox=False):
         self._client = gobiko.apns.APNsClient(
-            team_id="S4WXAUZQEV",
-            bundle_id="uk.co.inseven.status-panel",
-            auth_key_id="V5XKL2D8B9",
-            auth_key=os.environ['JWT_PRIVATE_KEY'],
+            team_id=os.environ['APNS_TEAM_ID'],
+            bundle_id=os.environ['APNS_BUNDLE_ID'],
+            auth_key_id=os.environ['ANPS_KEY_ID'],
+            auth_key=os.environ['APNS_KEY'],
             use_sandbox=use_sandbox,
         )
 
     def send_keepalive(self, device_tokens):
-        self._client.send_bulk_message(
-            device_tokens,
-            "Amiga Forever",
-            content_available=True,
-        )
+        try:
+            self._client.send_bulk_message(
+                device_tokens,
+                "Amiga Forever",
+                content_available=True,
+            )
+        except gobiko.apns.exceptions.PartialBulkMessage as e:
+            # If we encounter an error, we attempt to send a message to each device individually
+            # to provide greater visibility of the individual errors.
+            bad_tokens = []
+            print(f"Failed to bulk send notifications to device tokens: {e.bad_registration_ids}")
+            for device_token in e.bad_registration_ids:
+                print(f"Trying to send message to '{device_token}'...")
+                try:
+                    self._client.send_message(
+                        device_token,
+                        "Amiga Forever",
+                        content_available=True,
+                    )
+                    print("Success!")
+                except gobiko.apns.exceptions.BadDeviceToken:
+                    print(f"Failed to send notification to bad device token '{device_token}'.")
+                    bad_tokens.append(device_token)
+            if bad_tokens:
+                raise BadTokens(tokens=bad_tokens)
