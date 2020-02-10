6a2b1ce36a2c14a179f93330c579dd87127c1187
Jason Barrie Morley <jason.morley@inseven.co.uk>
2020-02-09 22:20:03 -0800

    Better support for sending APNS notifications locally (for debugging), and improved APNS error handling.

--- service/task.py
+++ service/task.py
@@ -1,21 +1,37 @@
 #!/usr/bin/env python3
 
+import argparse
 import subprocess
 
 import apns
 import database
 
 
-db = database.Database()
+def main():
+    parser = argparse.ArgumentParser(description="Periodic task for StatusPanel.")
+    parser.add_argument('--database-url', default=None, help="use alternative database URL; the DATABASE_URL environment variable will be used otherwise")
+    options = parser.parse_args()
 
-# Delete any devices that haven't been seen in a month.
-print("Purging stale devices...")
-db.purge_stale_devices(max_age=60 * 60 * 24 * 30)
+    # Connect to the database.
+    db = database.Database(database_url=options.database_url)
 
-# Send the tokens.
-print("Sending keepalive...")
-tokens = db.get_devices()
-if tokens:
-    print(f"Devices: {tokens}")
-    client = apns.APNS()
-    client.send_keepalive(device_tokens=tokens)
+    # Delete any devices that haven't been seen in a month.
+    print("Purging stale devices...")
+    db.purge_stale_devices(max_age=60 * 60 * 24 * 30)
+
+    # Send the tokens.
+    print("Sending keepalive...")
+    tokens = db.get_devices()
+    if tokens:
+        print(f"Devices: {tokens}")
+        client = apns.APNS()
+        try:
+            client.send_keepalive(device_tokens=tokens)
+        except apns.BadTokens as e:
+            for device_token in e.tokens:
+                print(f"Cleaning up device '{device_token}'...")
+                db.delete_device(token=device_token)
+
+
+if __name__=="__main__":
+    main()
