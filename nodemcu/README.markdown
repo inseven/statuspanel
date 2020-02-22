ac0a514efa0e949868b235b6aa6ab53c0e78ee7e
Tom Sutcliffe <tomsci@me.com>
2020-02-22 21:02:41 +0000

    Fixing all the things!
    Refactored stuff into main.lua to make things more resilient to errors
    tanking the board.
    
    Moved UnpairPin to GPIO32 and support wake-on-gpio for it and UsbDetect.
    Added logic for distinguishing short and long presses on UnpairPin.
    
    If UsbDetect is high, board sleep period will only be 10 seconds.

--- nodemcu/README.markdown
+++ nodemcu/README.markdown
@@ -10,7 +10,7 @@ Once you've installed the scripts, you can connect to the board using `minicom` 
 
 The Arduino IDE can be a convenient way to communicate with the device. You can find out more about setting this up on the [Adafruit website](https://learn.adafruit.com/adafruit-huzzah32-esp32-feather/using-with-arduino-ide).
 
-The firmware will automatically start if GPIO 14 is held high. This can be done by shorting it to the 3.3V pin. For debugging purposes, you may wish to leave it low and execute the code directly from the serial console:
+The firmware will automatically start if GPIO 14 is held high. This can be done by shorting it to the 3.3V pin. For debugging purposes, you may wish to leave it low (or disconnected) and execute the code directly from the serial console:
 
 ```lua
 initp(displayRegisterScreen) -- To show enrollment QR code
@@ -89,7 +89,7 @@ vs Arduino:
 
     esptool.py --chip esp32 --port /dev/cu.SLAB_USBtoUART --baud 921600 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 40m --flash_size detect 0x1000 ~/Documents/Dev/nodemcu/esp32/build/bootloader/bootloader.bin 0x10000 ~/Documents/Dev/nodemcu/esp32/build/NodeMCU.bin 0x8000 ~/Documents/Dev/nodemcu/esp32/build/partitions_tomsci.bin
 
-## Updating LUA Script
+## Updating Lua scripts
 
 Before getting started, you'll have to install the Python dependencies:
 
@@ -109,27 +109,18 @@ pipenv run python3 ../nodemcu-uploader/nodemcu-uploader.py \
     upload init.lua panel.lua network.lua rle.lua font.lua root.pem
 ```
 
-Not sure what this does:
+## How things were made
+
+### root.pem
+
+Is the root cert that statuspanel.io is using, created by:
 
 ```bash
-make menuconfig
-make MORE_CFLAGS="-DLUA_NUMBER_INTEGRAL"
+openssl x509 -inform der -outform pem -in "Baltimore CyberTrust Root.cer" -out root.pem
 ```
 
----
+### The ROM image
 
-openssl x509 -inform der -outform pem -in "Baltimore CyberTrust Root.cer" -out root.pem
-
-
-TODO:
-
-Error in tmr_register /Users/tomsci/Documents/Dev/nodemcu/esp32/components/modules/tmr.c if interval < configured configTICK_RATE_HZ (from CONFIG_FREERTOS_HZ) rather than asserting
+drop sdkconfig in, run `make`.
 
 Last known good dev-esp32: bf58084
-
-
-lcross fix issue:
-https://github.com/nodemcu/nodemcu-firmware/issues/1977
-tomsci_fix_crosscompiler
-
-
---
+++ nodemcu/main.lua
@@ -0,0 +1,134 @@
+require "network"
+
+-- It takes about 2 seconds for board to boot so the actual time required to
+-- long press is about LongPressTime plus 2 seconds.
+
+LongPressTime = 2000 -- milliseconds
+unpairPressTimer = nil
+
+function unpairPressed()
+    local pressed = gpio.read(UnpairPin) == 1
+    if not pressed then
+        -- Already released
+        shortPressUnpair()
+    else
+        unpairDownTime = node.uptime()
+        unpairPressTimer = tmr.create()
+        unpairPressTimer:alarm(LongPressTime, tmr.ALARM_SINGLE, timerExpired)
+        gpio.trig(UnpairPin, gpio.INTR_DOWN, unpairReleased)
+    end
+end
+
+function timerExpired()
+    unpairPressTimer = nil
+    if unpairDownTime then
+        unpairDownTime = nil
+        longPressUnpair()
+    end
+end
+
+function unpairReleased()
+    if unpairPressTimer then
+        unpairPressTimer:unregister()
+        unpairPressTimer = nil
+    end
+
+    if unpairDownTime then
+        local delta = node.uptime() - unpairDownTime
+        unpairDownTime = nil
+        if delta >= LongPressTime * 1000 then
+            longPressUnpair()
+        else
+            shortPressUnpair()
+        end
+    end
+end
+
+function shortPressUnpair()
+    print("Short press on unpair")
+end
+
+function longPressUnpair()
+    print("Long press on unpair")
+    setStatusLed(1)
+end
+
+function sleepFromDate(date, wakeTime)
+    -- hugely hacky date calculations, just the absolute worst
+    -- Epoch is midnight this morning
+    -- print(date, wakeTime)
+    local h, m, s = date:match("(%d%d):(%d%d):(%d%d)")
+    h, m, s = tonumber(h), tonumber(m), tonumber(s)
+    local now = (((h * 60) + m) * 60) + s
+
+    -- wakeTime is in minutes, target is in seconds
+    local target = wakeTime and wakeTime * 60 or (6 * 60 + 20) * 60
+    if target < now then
+        target = target + 24 * 60 * 60
+    end
+
+    local delta = target - now
+    if gpio.read(UsbDetect) == 1 then
+        delta = 10
+    end
+    print(string.format("Sleeping for %d secs (~%d hours)", delta, math.floor(delta / (60*60))))
+    wifi.stop()
+    node.dsleeps(delta, { UnpairPin, UsbDetect, pull=true })
+end
+
+-- For testing
+function slp()
+    node.dsleeps(60, { UsbDetect, UnpairPin, pull=true })
+end
+
+-- Sigh, bit library is 32-bit only
+function isset64(num, bitnum)
+    if bitnum >= 32 then
+        bitnum = bitnum - 32
+        num = num / 0x100000000
+    end
+    return bit.isset(num, bitnum)
+end
+
+function main(autoMode)
+    local wokeByUsb, wokeByUnpair
+    local reason, ext, pins = node.bootreason()
+    if ext == 5 then -- Deep sleep wake
+        if isset64(pins, UnpairPin) then
+            wokeByUnpair = true
+        elseif isset64(pins, UsbDetect) then
+            wokeByUsb = true
+        end
+    end
+
+    if not autoMode then
+        print("To show enrollment QR code: initp(displayRegisterScreen)")
+        print("To fetch latest image: getImg()")
+        print("To display last-fetched image: initp(displayStatusImg)")
+        if wokeByUsb then
+            print("Woke up by UsbDetect (A3)")
+        end
+        if wokeByUnpair then
+            print("Woke up by UnpairPin (32)")
+        end
+    end
+
+    local shouldFetchImage = autoMode
+
+    if wokeByUnpair then
+        shouldFetchImage = false
+        unpairPressed()
+    end
+
+    wifi.mode(wifi.STATION)
+    wifi.sta.on("got_ip", function(name, event)
+        print("Got IP "..event.ip)
+        ip = event.ip
+        gw = event.gw
+        if shouldFetchImage then
+            fetch()
+        end
+    end)
+    wifi.start()
+    wifi.sta.connect()
+end
