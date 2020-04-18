f92ddb912a99ba100d351eed8dcd44887204eb93
Tom Sutcliffe <tomsci@me.com>
2020-04-18 12:31:27 +0100

    Switched to LUA_NUMBER_INTEGRAL build
    and updated bootreason() code to match the fixed API.

--- nodemcu/main.lua
+++ nodemcu/main.lua
@@ -120,23 +120,23 @@ function slpdbg()
     getImg(completion)
 end
 
--- Sigh, bit library is 32-bit only
-function isset64(num, bitnum)
+function isset64(numlo, numhi, bitnum)
+    local num = numlo
     if bitnum >= 32 then
         bitnum = bitnum - 32
-        num = num / 0x100000000
+        num = numhi
     end
     return bit.isset(num, bitnum)
 end
 
 function main(autoMode)
     local wokeByUsb, wokeByUnpair
-    local reason, ext, pins = node.bootreason()
+    local reason, ext, pinslo, pinshi = node.bootreason()
     if ext == 5 then -- Deep sleep wake
-        if not pins then pins = 0 end
-        if isset64(pins, UnpairPin) then
+        if not pinshi then pinslo, pinshi = 0, 0 end
+        if isset64(pinslo, pinshi, UnpairPin) then
             wokeByUnpair = true
-        elseif isset64(pins, UsbDetect) then
+        elseif isset64(pinslo, pinshi, UsbDetect) then
             wokeByUsb = true
         end
     end
