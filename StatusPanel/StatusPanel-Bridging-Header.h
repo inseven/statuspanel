be20da698ff7a3080b37221a942857298435cdb6
Tom Sutcliffe <tomsci@me.com>
2020-05-17 21:28:46 +0100

    Added WifiProvisionerController

---
+++ StatusPanel/StatusPanel-Bridging-Header.h
@@ -0,0 +1,6 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import <SystemConfiguration/CaptiveNetwork.h>
+//#import <NetworkExtension/NEHotspotConfigurationManager.h>
