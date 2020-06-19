8ea3efc2407d140c3ffad2a95ebd78d17fcf5d8c
Tom Sutcliffe <tomsci@me.com>
2020-06-19 20:56:03 +0100

    Updated LFS

--- nodemcu/make_lfs.sh
+++ nodemcu/make_lfs.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-~/dev/esp32/build/luac_cross/luac.cross -o esp32/lfs.img -a 0x3F440000 -m 0x10000 *.lua
+~/dev/esp32/build/luac_cross/luac.cross -o esp32/lfs.img -a 0x3F440000 -m 0x20000 *.lua
