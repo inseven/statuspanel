473c103a33cdaca760047643f44149ee7fdc7584
Jason Barrie Morley <jbmorley@mac.com>
2021-04-21 16:44:57 -0700

    Much build fun:
    - Use the Swift Package Manager for Sodium
    - Catch up to the new Sodium (the old one stopped building after the ARM Mac support was added to Xcode 😢)
    - Add a build script in advance of GitHub Actions builds

---
+++ scripts/build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+set -x
+
+SCRIPT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+ROOT_DIRECTORY="${SCRIPT_DIRECTORY}/.."
+
+# TODO: Re-enable test builds if possible using a locally generated signing key.
+
+IPHONE_DESTINATION="platform=iOS Simulator,name=iPhone 12 Pro,OS=14.4"
+
+function build_scheme {
+    xcodebuild \
+        -project StatusPanel.xcodeproj \
+        -scheme "$1" \
+        CODE_SIGN_IDENTITY="" \
+        CODE_SIGNING_REQUIRED=NO \
+        CODE_SIGNING_ALLOWED=NO "${@:2}" | xcpretty
+}
+
+cd "$ROOT_DIRECTORY"
+
+# List the available devices for future reference (predominantly for build servers)
+xcrun instruments -s devices
+
+build_scheme "StatusPanel" clean build \
+  -sdk iphonesimulator \
+  -destination "$IPHONE_DESTINATION"
