b760dca726c7c36093e0e2f4a7de791baa0d64ed
Jason Barrie Morley <jbmorley@mac.com>
2021-09-22 08:06:54 -0700

    Add a TODO.

--- scripts/environment.sh
+++ scripts/environment.sh
@@ -20,6 +20,8 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+# TODO: Use local installs of Python and Ruby #170
+#       https://github.com/inseven/statuspanel/issues/170
 export PATH="/opt/homebrew/bin":$PATH
 export PATH="/opt/homebrew/opt/ruby/bin":$PATH
 
