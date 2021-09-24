bced1dd44a9844f994c3351aded7d1ca296f0ef5
Jason Barrie Morley <jbmorley@mac.com>
2021-09-23 17:08:38 -0700

    Use the Xcode path as set by a runner-specific environment variable.

--- scripts/build.sh
+++ scripts/build.sh
@@ -103,11 +103,8 @@ cd "$APP_DIRECTORY"
 echo $APP_CONFIGURATION > "${APP_DIRECTORY}/StatusPanel/configuration.json"
 
 # Select the correct Xcode.
-IOS_XCODE_PATH=${IOS_XCODE_PATH:-CHEESE}
-echo "******************"
-echo $IOS_XCODE_PATH
-echo "******************"
-sudo xcode-select --switch /Applications/Xcode.app
+IOS_XCODE_PATH=${IOS_XCODE_PATH:/Applications/Xcode.app}
+sudo xcode-select --switch "$IOS_XCODE_PATH"
 
 # List the available schemes.
 xcode_project -list
