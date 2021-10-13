db93fc004bf71d9adc425252d146e9c7a539515d
Jason Barrie Morley <jbmorley@mac.com>
2021-10-13 13:35:44 -0700

    Whitespace change.

--- ios/StatusPanel/ViewController.swift
+++ ios/StatusPanel/ViewController.swift
@@ -131,7 +131,6 @@ class ViewController: UIViewController, SettingsViewControllerDelegate {
         }
     }
 
-
     func didDismiss(settingsViewController: SettingsViewController) {
         sourceController.fetch()
     }
