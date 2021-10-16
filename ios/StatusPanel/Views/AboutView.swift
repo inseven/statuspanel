2fff0bf288791eeebd60440438fd0876c798078b
Jason Morley <hello@jbmorley.co.uk>
2021-10-16 16:54:04 -0700

    fix(ios): Add the Swift-Sodium license to the About view (#265)

--- ios/StatusPanel/Views/AboutView.swift
+++ ios/StatusPanel/Views/AboutView.swift
@@ -72,7 +72,7 @@ struct AboutView: View {
                         Text(person)
                     }
                 }
-                Section(header: Text("With Thanks")) {
+                Section(header: Text("Thanks")) {
                     ForEach(people) { person in
                         Text(person)
                     }
@@ -83,15 +83,12 @@ struct AboutView: View {
                     }
                 }
                 Section(header: Text("Code Licenses")) {
-                    LicenseRow(name: "Binding+mappedToBool", author: "", license: """
-Copyright 2020 Joseph Duffy
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-""")
+                    LicenseRow(name: "Binding+mappedToBool",
+                               author: "Joseph Duffy",
+                               license: String(contentsOfBundleFile: "Binding+mappedToBool") ?? "missing file")
+                    LicenseRow(name: "Swift-Sodium",
+                               author: "Frank Denis",
+                               license: String(contentsOfBundleFile: "Swift-Sodium") ?? "missing file")
                 }
             }
             .navigationBarTitle("About", displayMode: .inline)
