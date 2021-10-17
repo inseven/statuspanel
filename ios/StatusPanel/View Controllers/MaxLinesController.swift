134137e667534b462ce1dabb1dfcad4cf7b883d8
Jason Morley <hello@jbmorley.co.uk>
2021-10-17 05:31:21 -0700

    refactor(ios): Move max lines controller out of the storyboard (#276)

--- ios/StatusPanel/View Controllers/MaxLinesController.swift
+++ ios/StatusPanel/View Controllers/MaxLinesController.swift
@@ -19,39 +19,64 @@
 // SOFTWARE.
 
 import UIKit
+import SwiftUI
 
 class MaxLinesController : UITableViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        title = "Maximum Lines"
+    private static let cellReuseIdentifier = "Cell"
+
+    private var config: Config
+
+    init(config: Config) {
+        self.config = config
+        super.init(style: .grouped)
+        tableView.allowsSelection = true
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: Self.cellReuseIdentifier)
+        title = LocalizedString("maximum_lines_title")
     }
 
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        let val = Config().maxLines
-        updateTickMark(val)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 
-    func numItems() -> Int {
-        return self.tableView.numberOfRows(inSection: 0)
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
 
-    func updateTickMark(_ tag: Int) {
-        for i in 0 ..< numItems() {
-            let cell = self.tableView.cellForRow(at: IndexPath(row: i, section: 0))
-            cell?.accessoryType = (cell?.tag == tag) ? .checkmark : .none
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+
+    func configValue(forRowAt indexPath: IndexPath) -> Int {
+        switch indexPath.row {
+        case 0:
+            return 1
+        case 1:
+            return 2
+        case 2:
+            return 3
+        case 3:
+            return 4
+        default:
+            return 0
         }
     }
 
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: Self.cellReuseIdentifier, for: indexPath)
+        let configValue = self.configValue(forRowAt: indexPath)
+        cell.textLabel?.text = configValue == 0 ? LocalizedString("maximum_lines_unlimited_label") : String(configValue)
+        cell.accessoryType = config.maxLines == configValue ? .checkmark : .none
+        return cell
+    }
+
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let tag = tableView.cellForRow(at: indexPath)!.tag
-        Config().maxLines = tag
-        tableView.performBatchUpdates({
+        config.maxLines = configValue(forRowAt: indexPath)
+        tableView.performBatchUpdates {
             tableView.deselectRow(at: indexPath, animated: true)
-            updateTickMark(tag)
-        }, completion: { finished in
+            tableView.reloadData()
+        } completion: { finished in
             self.navigationController?.popViewController(animated: true)
-        })
+        }
     }
 }
