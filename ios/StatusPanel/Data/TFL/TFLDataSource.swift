0d12b0d41ae9f71dddb8f0e679c3944e61c6a0f8
Jason Barrie Morley <jbmorley@mac.com>
2021-10-19 20:00:20 -0700

    Move the Color extension into a separate file.

--- ios/StatusPanel/Data/TFL/TFLDataSource.swift
+++ ios/StatusPanel/Data/TFL/TFLDataSource.swift
@@ -22,16 +22,6 @@ import Foundation
 import UIKit
 import SwiftUI
 
-extension Color {
-
-    init(hex: Int, opacity: Double = 1.0) {
-        let red = Double((hex & 0xff0000) >> 16) / 255.0
-        let green = Double((hex & 0xff00) >> 8) / 255.0
-        let blue = Double((hex & 0xff) >> 0) / 255.0
-        self.init(.sRGB, red: red, green: green, blue: blue, opacity: opacity)
-    }
-}
-
 // See https://api-portal.tfl.gov.uk/admin/applications/1409617922524
 final class TFLDataSource: DataSource {
 
