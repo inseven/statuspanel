8a1a5aa51473e62b10e0cda75c065bb3dbb920f4
Jason Morley <hello@jbmorley.co.uk>
2021-10-22 15:09:05 -0700

    ci: Use a custom Docker file for firmware builds (#319)
    This change introduces a dedicated, lightweight, Dockerfile for firmware builds and fixes some bugs with previous builds resulting from being unable to pass custom build flags.

---
+++ firmware/docker/docker-build.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Copyright (c) 2018-2021 Jason Morley, Tom Sutcliffe
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+set -e
+set -o pipefail
+set -x
+set -u
+
+DOCKER_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+ROOT_DIRECTORY="$DOCKER_DIRECTORY"/../..
+
+DOCKER_IMAGE_TAG=statuspanel-nodemcu-build
+
+cd "$DOCKER_DIRECTORY"
+
+# Build the docker image.
+docker build . -t $DOCKER_IMAGE_TAG
+IMAGE_ID=$( docker images -q $DOCKER_IMAGE_TAG )
+
+# Build the firmware.
+docker run --volume "$ROOT_DIRECTORY:/opt/statuspanel" $IMAGE_ID /opt/statuspanel/firmware/docker/build.sh
