74954ba4732245a6d201095d617cd13735474127
Pavlos Vinieratos <pvinis@gmail.com>
2023-02-28 23:13:11 +0000

    images missing

--- react-sim/src/pages/index.tsx
+++ react-sim/src/pages/index.tsx
@@ -1,7 +1,8 @@
 import { type NextPage } from "next"
-import Link from "next/link"
 import { useSodium } from "../utils/sodium"
 import QRCode from "react-qr-code"
+import { StreamDataView } from "stream-data-view"
+import { forEach, pipe, range, reverse } from "remeda"
 
 const Home: NextPage = () => {
 	const sodium = useSodium()
@@ -11,38 +12,78 @@ const Home: NextPage = () => {
 	}
 
 	const keypair = sodium.crypto_box_keypair()
+	const pubBase64 = sodium.to_base64(
+		keypair.publicKey,
+		sodium.base64_variants.ORIGINAL
+	)
+	const id = "reactidd"
+	const url = `statuspanel:r2?id=${id}&pk=${encodeURIComponent(pubBase64)}`
+
+	const update = async () => {
+		const ab = await (
+			await fetch(`https://api.statuspanel.io/api/v2/${id}`)
+		).arrayBuffer()
+
+		const data = new StreamDataView(ab, true)
+		const marker = data.getNextUint16()
+		if (marker !== 0xff00) {
+			console.log("invalid marker")
+			return
+		}
+		const headerLength = data.getNextUint8()
+		const wakeupTime = data.getNextUint16()
+		let imageCount = null
+		if (headerLength >= 6) {
+			imageCount = data.getNextUint8()
+		}
+
+		const offsets = []
+		if (imageCount === null) {
+			offsets.push(0)
+		} else {
+			pipe(
+				range(0, imageCount),
+				forEach(() => {
+					offsets.push(swap32(data.getNextUint32()))
+				})
+			)
+		}
+
+		const ranges: Array<[number, number]> = []
+		pipe(
+			offsets,
+			reverse,
+			forEach.indexed((offset: number, i) => {
+				ranges.push([offset, i === 0 ? data.getLength() : ranges[i - 1]![0]])
+			}),
+			() => ranges.reverse()
+		)
+
+		const images = []
+		ranges.forEach(([start, end]) => {
+			const length = end - start
+			const imageData = data.getBytes(start, length)
+		})
+	}
 
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
 			<div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-				<h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-					ready {sodium !== undefined ? "yes" : "no"}
-					keypair {keypair.publicKey} {keypair.privateKey}
-				</h1>
-				<QRCode value="hi" />
-				<div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-					<Link
-						className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-						href="https://create.t3.gg/en/usage/first-steps"
-						target="_blank"
-					>
-						<h3 className="text-2xl font-bold">First Steps →</h3>
-					</Link>
-					<Link
-						className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-						href="https://create.t3.gg/en/introduction"
-						target="_blank"
-					>
-						<h3 className="text-2xl font-bold">Documentation →</h3>
-						<div className="text-lg">
-							Learn more about Create T3 App, the libraries it uses, and how to
-							deploy it.
-						</div>
-					</Link>
-				</div>
+				<p className="text-white">{url}</p>
+				<QRCode value={url} />
+				<button onClick={() => void update()}>update</button>
 			</div>
 		</main>
 	)
 }
 
 export default Home
+
+function swap32(val: number) {
+	return (
+		((val & 0xff) << 24) |
+		((val & 0xff00) << 8) |
+		((val >> 8) & 0xff00) |
+		((val >> 24) & 0xff)
+	)
+}
