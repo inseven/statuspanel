508f60543362101b2a67e0ac507dbd61cee7a18b
Pavlos Vinieratos <pvinis@gmail.com>
2023-02-28 20:54:05 +0000

    qr

--- react-sim/src/pages/index.tsx
+++ react-sim/src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { type NextPage } from "next"
 import Link from "next/link"
 import { useSodium } from "../utils/sodium"
+import QRCode from "react-qr-code"
 
 const Home: NextPage = () => {
 	const sodium = useSodium()
@@ -18,6 +19,7 @@ const Home: NextPage = () => {
 					ready {sodium !== undefined ? "yes" : "no"}
 					keypair {keypair.publicKey} {keypair.privateKey}
 				</h1>
+				<QRCode value="hi" />
 				<div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
 					<Link
 						className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
