6d50055f34255f030c480a8a72db5cc2b3b4eadc
Jason Morley <hello@jbmorley.co.uk>
2023-03-28 14:14:16 +0100

    Move the bulk of the rendering code into `Renderer`

--- ios/StatusPanel/View Controllers/ViewController.swift
+++ ios/StatusPanel/View Controllers/ViewController.swift
@@ -189,10 +189,10 @@ class ViewController: UIViewController, SettingsViewControllerDelegate {
         let darkMode = Config().displaysInDarkMode
 
         // TODO fix this, for now always show eink format in the UI regardless of what devices are enrolled
-        let image = Self.renderToImage(data: data, shouldRedact: false, darkMode: darkMode, deviceType: .einkV1)
+        let image = Renderer.renderToImage(data: data, shouldRedact: false, darkMode: darkMode, deviceType: .einkV1)
         let privacyImage = ((Config().privacyMode == .customImage)
                             ? ViewController.loadPrivacyImage()
-                            : Self.renderToImage(data: data, shouldRedact: true, darkMode: darkMode, deviceType: .einkV1))
+                            : Renderer.renderToImage(data: data, shouldRedact: true, darkMode: darkMode, deviceType: .einkV1))
         let payloads = Panel.rlePayloads(for: [image, privacyImage])
         self.fetchDidUpdate(image: payloads[0].1, privacyImage: payloads[1].1)
 
@@ -202,13 +202,13 @@ class ViewController: UIViewController, SettingsViewControllerDelegate {
         var anyChanges = false
         for device in devices {
             var images: [UIImage] = []
-            let primary = Self.renderToImage(data: data, shouldRedact: false, darkMode: darkMode, deviceType: device.kind)
+            let primary = Renderer.renderToImage(data: data, shouldRedact: false, darkMode: darkMode, deviceType: device.kind)
             images.append(primary)
             if device.kind == .einkV1 {
                 // Privacy image only really makes sense on eink
                 let privacyImage = (config.privacyMode == .customImage)
                 ? ViewController.loadPrivacyImage()
-                : Self.renderToImage(data: data, shouldRedact: true, darkMode: darkMode, deviceType: device.kind)
+                : Renderer.renderToImage(data: data, shouldRedact: true, darkMode: darkMode, deviceType: device.kind)
                 images.append(privacyImage)
             } else {
                 // TODO support multiple pages, or something
@@ -235,162 +235,6 @@ class ViewController: UIViewController, SettingsViewControllerDelegate {
 
     static func loadPrivacyImage() -> UIImage {
         return (try? Config().privacyImage()) ?? Panel.blankImage()
-    }
-
-    static func renderToImage(data: [DataItemBase], shouldRedact: Bool, darkMode: Bool, deviceType: Device.Kind) -> UIImage {
-        let contentView = UIView(frame: CGRect(origin: .zero, size: Device.sizeFor(kind: deviceType)))
-        contentView.contentScaleFactor = 1.0
-
-        // Construct the contentView's contents. For now just make labels and flow them into 2 columns
-        contentView.backgroundColor = darkMode ? UIColor.black : UIColor.white
-        let foregroundColor = darkMode ? UIColor.white : UIColor.black
-        let config = Config()
-        let twoCols = contentView.frame.size.width < 500 ? false : config.displayTwoColumns
-        let showIcons = config.showIcons
-        let rect = contentView.frame
-        let maxy = rect.height - 10 // Leave space for status line
-        let midx = rect.width / 2
-        var x : CGFloat = 5
-        var y : CGFloat = 0
-        let colWidth = twoCols ? (rect.width / 2 - x * 2) : rect.width - x
-        let bodyFont = config.getFont(named: config.bodyFont)
-        let itemGap = deviceType == .featherTft ? 4 : CGFloat(min(10, bodyFont.textHeight / 2)) // ie 50% of the body text line height up to a max of 10px
-        var colStart = y
-        var col = 0
-        var columnItemCount = 0 // Number of items assigned to the current column
-        var divider: Renderer.DividerStyle? = twoCols ? .vertical(originY: 0) : nil
-        let redactMode: RedactMode = (shouldRedact ? (config.privacyMode == .redactWords ? .redactWords : .redactLines) : .none)
-
-        for item in data {
-            
-            let flags = item.flags
-            let font = flags.contains(.header) ? config.titleFont : config.bodyFont
-            let labelStyle = deviceType == .featherTft ? .subText : flags.labelStyle
-            let fontDetails = Config().getFont(named: font)
-            let w = flags.contains(.spansColumns) ? rect.width : colWidth
-            let frame = CGRect(x: x, y: y, width: w, height: 0)
-            let view = UIView(frame: frame)
-            var prefix = fontDetails.supportsEmoji && showIcons ? item.iconAndPrefix : item.prefix
-            let numPrefixLines = prefix.split(separator: "\n").count
-            var textFrame = CGRect(origin: CGPoint.zero, size: frame.size)
-            var itemHeight: CGFloat = 0
-
-            if !prefix.isEmpty {
-                let prefixLabel = UILabel.getLabel(frame: textFrame,
-                                                   font: font,
-                                                   style: labelStyle,
-                                                   redactMode: redactMode)
-                prefixLabel.textColor = foregroundColor
-                prefixLabel.numberOfLines = numPrefixLines
-                prefixLabel.text = prefix + " "
-                prefixLabel.sizeToFit()
-                let prefixWidth = prefixLabel.frame.width
-                if prefixWidth < frame.width / 2 {
-                    prefix = ""
-                    view.addSubview(prefixLabel)
-                    textFrame = textFrame.divided(atDistance: prefixWidth, from: .minXEdge).remainder
-                    itemHeight = prefixLabel.frame.height
-                } else {
-                    // Label too long, treat as single text entity (leave 'prefix' set)
-                    prefix = prefix + " "
-                }
-            }
-            let label = UILabel.getLabel(frame: textFrame,
-                                         font: font,
-                                         style: labelStyle,
-                                         redactMode: redactMode)
-            label.numberOfLines = 1 // Temporarily while we're using it in checkFit
-
-            let text = prefix + item.getText(checkFit: { (string: String) -> Bool in
-                label.text = prefix + string
-                let size = label.sizeThatFits(textFrame.size)
-                return size.width <= textFrame.width
-            })
-            label.textColor = foregroundColor
-            if flags.contains(.warning) {
-                // Icons don't render well on the panel, use a coloured background instead
-                label.backgroundColor = UIColor.yellow
-                label.textColor = UIColor.black
-            }
-            label.numberOfLines = config.maxLines
-            label.lineBreakMode = .byTruncatingTail
-            label.text = text
-            label.sizeToFit()
-            itemHeight = max(itemHeight, label.bounds.height)
-            label.frame = CGRect(x: label.frame.minX, y: label.frame.minY, width: textFrame.width, height: label.frame.height)
-            view.frame = CGRect(origin: view.frame.origin, size: CGSize(width: view.frame.width, height: itemHeight))
-            view.addSubview(label)
-            if let subText = item.subText {
-                let subLabel = UILabel.getLabel(frame: textFrame,
-                                                font: font,
-                                                style: .subText,
-                                                redactMode: redactMode)
-                subLabel.textColor = foregroundColor
-                subLabel.numberOfLines = config.maxLines
-                subLabel.text = subText
-                subLabel.sizeToFit()
-                subLabel.frame = CGRect(x: textFrame.minX, y: view.bounds.maxY + 1, width: textFrame.width, height: subLabel.frame.height)
-                view.frame = CGRect(origin: view.frame.origin, size: CGSize(width: view.frame.width, height: subLabel.frame.maxY))
-                view.addSubview(subLabel)
-            }
-            let sz = view.frame
-            // Enough space for this item?
-            let itemIsColBreak = (columnItemCount > 0 && flags.contains(.prefersEmptyColumn))
-            if (col == 0 && twoCols && (sz.height > maxy - y || itemIsColBreak)) {
-                // overflow to 2nd column
-                col += 1
-                columnItemCount = 0
-                x += midx + 5
-                y = colStart
-                view.frame = CGRect(x: x, y: y, width: sz.width, height: sz.height)
-            } else if (!twoCols && itemIsColBreak) {
-                // Leave some extra space and mark where to draw a line
-                divider = .horizontal(originY: y)
-                let c = view.center
-                view.center = CGPoint(x: c.x, y: c.y + itemGap)
-                y += itemGap
-            }
-            contentView.addSubview(view)
-
-            y = y + sz.height + itemGap
-
-            if flags.contains(.spansColumns) {
-                // Update the verticial origin of the divider and columns, and start at column 0 again.
-                divider = .vertical(originY: y)
-                colStart = y
-                columnItemCount = 0
-                col = 0
-            } else {
-                // Track the number of items in the current column.
-                columnItemCount += 1
-            }
-
-        }
-
-        // And render it into an image
-        let result = UIImage.New(rect.size, flipped: false) { context in
-            // layer.render() works when the device is locked, whereas drawHierarchy() doesn't
-            contentView.layer.render(in: context)
-
-            // Draw the dividing line.
-            if let divider = divider {
-
-                context.setStrokeColor(foregroundColor.cgColor)
-                context.beginPath()
-
-                switch divider {
-                case .vertical(let originY):
-                    context.move(to: CGPoint(x: midx, y: originY))
-                    context.addLine(to: CGPoint(x: midx, y: rect.height - Panel.statusBarHeight))
-                case .horizontal(let originY):
-                    context.move(to: CGPoint(x: x, y: originY))
-                    context.addLine(to: CGPoint(x: rect.width - x, y: originY))
-                }
-
-                context.drawPath(using: .stroke)
-            }
-        }
-        return result
     }
 
     override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
