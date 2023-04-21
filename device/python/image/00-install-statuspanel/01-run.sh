4c12f6ad59ca375f637505748077bc4f0dc47943
Jason Morley <hello@jbmorley.co.uk>
2023-04-21 13:39:44 -0600

    Add the StatusPanel stage

---
+++ device/python/image/00-install-statuspanel/01-run.sh
@@ -0,0 +1,50 @@
+#!/bin/bash -e
+
+# Enable I2C and SPI.
+cat <<EOF>> "${ROOTFS_DIR}/boot/config.txt"
+dtparam=i2c_arm=on
+dtparam=spi=on
+EOF
+
+# Autoload the I2C device (enabled in stage1/00-boot-files/files/config.txt).
+cat <<EOF>> "${ROOTFS_DIR}/etc/modules"
+i2c-dev
+EOF
+
+# Install Pimoroni Inky library.
+on_chroot <<EOF
+cd /opt
+git clone --depth 1 https://github.com/pimoroni/inky.git
+cd inky
+./install.sh
+EOF
+
+# Install StatusPanel Python dependencies.
+on_chroot <<EOF
+pip3 install pysodium
+EOF
+
+# Install StatusPanel.
+on_chroot <<EOF
+cd /opt
+git clone --depth 1 https://github.com/inseven/statuspanel.git
+EOF
+
+# Install the StatusPanel service.
+cat <<EOF> "${ROOTFS_DIR}/etc/systemd/system/statuspanel.service"
+[Unit]
+
+Description=StatusPanel
+After=network.target
+
+[Service]
+ExecStart=/usr/bin/python3 /opt/statuspanel/device/python/device.py
+User=root
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+on_chroot <<EOF
+systemctl enable statuspanel.service
+EOF
