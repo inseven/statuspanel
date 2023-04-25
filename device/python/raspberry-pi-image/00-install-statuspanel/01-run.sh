4e8c4a99d2c3621c36a5b834fac46ecee699bd33
Jason Morley <hello@jbmorley.co.uk>
2023-04-25 05:21:38 -0600

    feat(device-python): Generate Raspberry Pi images (#479)

---
+++ device/python/raspberry-pi-image/00-install-statuspanel/01-run.sh
@@ -0,0 +1,43 @@
+#!/bin/bash -e
+
+# Enable I2C and SPI.
+cat <<EOF>> "${ROOTFS_DIR}/boot/config.txt"
+dtparam=i2c_arm=on
+dtparam=spi=on
+EOF
+
+# Autoload the I2C device (enabled in stage1/00-boot-files/files/config.txt).
+cat <<EOF>> "${ROOTFS_DIR}/etc/modules"
+i2c-dev
+EOF
+
+# Install Pimoroni Inky library.
+on_chroot <<EOF
+cd /opt
+git clone --depth 1 https://github.com/pimoroni/inky.git
+cd inky
+./install.sh
+EOF
+
+# Install StatusPanel.
+install -d "${ROOTFS_DIR}/opt/statuspanel"
+install -m 755 files/device.py "${ROOTFS_DIR}/opt/statuspanel/"
+
+# Install the StatusPanel service.
+cat <<EOF> "${ROOTFS_DIR}/etc/systemd/system/statuspanel.service"
+[Unit]
+
+Description=StatusPanel
+After=network.target
+
+[Service]
+ExecStart=/usr/bin/python3 /opt/statuspanel/device.py
+User=root
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+on_chroot <<EOF
+systemctl enable statuspanel.service
+EOF
