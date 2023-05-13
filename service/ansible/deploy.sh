523863be200e0dbb5b61611c456071240cba7ca2
Jason Morley <hello@jbmorley.co.uk>
2023-05-13 07:05:55 -0400

    Update the deploy script to install the Debian package

--- service/ansible/deploy.sh
+++ service/ansible/deploy.sh
@@ -7,6 +7,8 @@ SERVICE_DIRECTORY="${ANSIBLE_DIRECTORY}/.."
 
 SSH_KEY_FILE=ssh_key
 
+PACKAGE_PATH=`realpath $1`
+
 pushd "${ANSIBLE_DIRECTORY}"
 #echo -e "$ANSIBLE_SSH_KEY" > $SSH_KEY_FILE
 #chmod 0400 $SSH_KEY_FILE
@@ -14,6 +16,7 @@ ansible-playbook \
     -vv \
     -i hosts \
     statuspanel.yml \
+    --extra-vars package_path="${PACKAGE_PATH}" \
     --extra-vars ansible_ssh_private_key_file="${SSH_KEY_FILE}" \
     --ask-become-pass
 #rm -f "${SSH_KEY_FILE}"
