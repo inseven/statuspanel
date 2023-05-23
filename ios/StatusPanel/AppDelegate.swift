b2421ceb35a69296915c6f25bf16538d7214cbba
Jason Morley <hello@jbmorley.co.uk>
2023-05-23 15:35:49 -0400

    feat(ios): Show an error if background notification registration fails

--- ios/StatusPanel/AppDelegate.swift
+++ ios/StatusPanel/AppDelegate.swift
@@ -136,6 +136,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
         print("Aww, no APNS for us. Error: " + error.localizedDescription)
+        window?.rootViewController?.present(error: error)
     }
 
     func application(_ application: UIApplication,
